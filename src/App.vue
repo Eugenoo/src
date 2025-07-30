bd68056bd769d0f5c58506d5c0d7eb645e285ede
EugenOG <35118995+Eugenoo@users.noreply.github.com>
2025-07-30 15:47:16 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template class="bg-blue-300">
+  <div class="bg-blue-400">
+    <RouterView></RouterView>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from "@/components/navigation/Navbar.vue";
+</script>
+
+<template>
+  <navbar></navbar>
+
+</template>
+
+<style>
+
+</style>
